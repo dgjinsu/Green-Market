e1b363d5b10f8e3a5d409fe6e67b2ffdbadf9f5c
dgjinsu <rlawlstn3206@naver.com>
2024-08-08 18:49:07 +0900

    first commit

---
+++ green-market/green-market/src/main/java/green_market/GreenMarketApplication.java
@@ -0,0 +1,13 @@
+package green_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenMarketApplication.class, args);
+	}
+
+}
