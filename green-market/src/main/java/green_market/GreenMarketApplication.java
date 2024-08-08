c3ffd51643ce10dac598eb88d5eba96429022d88
dgjinsu <rlawlstn3206@naver.com>
2024-08-08 23:22:56 +0900

    feat: eureka server 등록

--- green-market/src/main/java/green_market/GreenMarketApplication.java
+++ green-market/src/main/java/green_market/GreenMarketApplication.java
@@ -2,8 +2,10 @@ package green_market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class GreenMarketApplication {
 
 	public static void main(String[] args) {
